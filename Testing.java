a4eb1a7f7f053acf57436a13c5f4693fb05233b8
rhula32 <rhula32@gmail.com>
2016-11-05 18:48:59 -0400

    Updated Main Method Class

--- Testing.java
+++ Testing.java
@@ -5,18 +5,18 @@ public class Testing
     Queue myQueue = new Queue();
     Stack myStack = new Stack();
     
-    System.out.println("THIS IS A DEMONSTRATION OF THE STACK CLASS: ");
+    //Push strings into a stack to create a stack & print
+    System.out.println("Demonstrating the stack operations: ");
     myStack.push("Magic");
     myStack.println();
-    myStack.push("Michael");
+    myStack.push("Scott");
     myStack.println();
-    myStack.push("Larry");
+    myStack.push("Worthy");
     myStack.println();
-    myStack.push("Duncan");
-    myStack.println();   
+    myStack.push("Rambis");
+    myStack.println();
     myStack.push("Abdul-Jabbar");
     myStack.println();
-    myStack.pop();
     myStack.println();
     myStack.pop();
     myStack.println();
@@ -26,33 +26,22 @@ public class Testing
     myStack.println();
     myStack.pop();
     myStack.println();
-    myStack.push("West");
+    myStack.pop();
     myStack.println();
-    myStack.push("Robertson");
-    myStack.println();
-    myStack.push("Baylor");
-    myStack.println();
-    myStack.push("Russell");
-    myStack.println();
-    myStack.push("Chamberlain");
-    myStack.println();
-    
     System.out.println();
     
-    System.out.println("THIS IS A DEMONSTRATION OF THE QUEUE CLASS: ");
-    myQueue.enqueue("Riley");
+    //Enqueue strings into a queue to create a queue & print
+    System.out.println("Demonstrating the queue operations: ");
+    myQueue.enqueue("Johnson");
     myQueue.println();
-    myQueue.enqueue("Jackson");
+    myQueue.enqueue("Ainge");
     myQueue.println();
-    myQueue.enqueue("Auerbach");
+    myQueue.enqueue("Bird");
     myQueue.println();
-    myQueue.enqueue("Kundla");
+    myQueue.enqueue("McHale");
     myQueue.println();
-    myQueue.enqueue("Popovich");
+    myQueue.enqueue("Parish");
     myQueue.println();
-    myQueue.dequeue();
-    myQueue.println();
-    myQueue.dequeue();
     myQueue.println();
     myQueue.dequeue();
     myQueue.println();
@@ -60,15 +49,49 @@ public class Testing
     myQueue.println();
     myQueue.dequeue();
     myQueue.println();
-    myQueue.enqueue("Wooden");
+    myQueue.dequeue();
     myQueue.println();
-    myQueue.enqueue("Rupp");
+    myQueue.dequeue();
     myQueue.println();
-    myQueue.enqueue("Knight");
-    myQueue.println();
-    myQueue.enqueue("Smith");
-    myQueue.println();
-    myQueue.enqueue("Coach K");
-    myQueue.println();
+    System.out.println();
+    
+    //Move strings from a stack to a queue & print in the same order
+    System.out.println("Moving the contents of the stack to a queue: ");
+    Queue newQueue = new Queue();
+    newQueue = stackToQueue(myStack);
+    newQueue.println();
+    
+    //Move strings from a queue to a stack & print in the same order
+    System.out.println("moving the contents of the queue to a stack: ");
+    Stack newStack = new Stack();
+    newStack = queueToStack(myQueue);
+    newStack.println();
+  }
+  
+  public static Queue stackToQueue(Stack stack)
+  {
+    Queue queue = new Queue();
+    while(!stack.isEmpty());
+    {
+      queue.enqueue(stack.pop().getString());
+    }
+    return(queue);
+  }
+  
+  public static Stack queueToStack(Queue queue)
+  {
+    Stack stack = new Stack();
+    while(!queue.isEmpty());
+    {
+      stack.push(queue.dequeue().getString());
+    }
+    return(stack);
+  }
+  
+  public static void stackToStack()
+  {
   }
 }
+  
+
+  
